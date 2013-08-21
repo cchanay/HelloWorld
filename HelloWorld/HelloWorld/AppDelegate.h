6deace880b6bb75052feb71c90b0680260692cad
Chelsea Chanay <cchanay@solstice-consulting.com>
2013-08-21 10:13:05 -0500

    Addition of iOS project files

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Chelsea Chanay on 8/21/13.
+//  Copyright (c) 2013 Chelsea Chanay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Chelsea Chanay on 8/21/13.
+//  Copyright (c) 2013 Chelsea Chanay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
